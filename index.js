266798e0291a2de3a8d869c5e2511b45caebafbb
ajitpsakri <sakriajit@gmail.com>
2021-08-18 16:42:45 +0530

    style update

--- index.js
+++ index.js
@@ -3,36 +3,46 @@ const numberOfShares = document.querySelector("#number-of-shares");
 const sellingPrice = document.querySelector("#stock-price-current")
 const submitButton = document.querySelector("#submit-btn");
 const message = document.querySelector("#message");
-
+const loading = document.querySelector("#loading");
+const leftDiv = document.querySelector(".left");
+const rightDiv = document.querySelector(".right");
+const leftMessage = document.querySelector("#left-message")
+const profitImage = document.querySelector("#profit-image")
+const lossImage = document.querySelector("#loss-image")
+console.log(leftMessage)
 submitButton.addEventListener('click', function () {
     if (costPrice.value > 0 && numberOfShares.value > 0 && sellingPrice.value > 0) {
-        //add loading svg
-
-        //message display none
-        message.style.display = "none"
+        loading.style.display = "block";
+        message.style.display = "none";
+        profitImage.style.display = "none";
+        lossImage.style.display = "none";
         setTimeout(function () {
-            //svg display none
-
-            //message display block
+            loading.style.display = "none"
             message.style.display = "block"
+
             let cost = Number(costPrice.value);
             let sell = Number(sellingPrice.value)
             let totalShares = Number(numberOfShares.value)
             let profit = sell - cost;
-            let profitPercentage = (profit / cost) * 100;
+            let profitPercentage = ((profit / cost) * 100).toFixed(2);
             let loss = cost - sell
-            let lossPercentage = (loss / cost) * 100;
+            let lossPercentage = ((loss / cost) * 100).toFixed(2);
             let moneyMade = profit * totalShares;
             let moneyLost = loss * totalShares;
             if (sell > cost) {
-                message.innerHTML = `You made 📈 ${moneyMade}₹ with profit percentage of ${profitPercentage}%`
+                message.innerHTML = `You made a profit of ${moneyMade}₹ and gained ${profitPercentage}%`
+                message.style.color = "#10B981";//green 
+                profitImage.style.display = "block";
             } else if (cost > sell) {
-                message.innerHTML = `You lost 📉 ${moneyLost}₹ with loss percentage of ${lossPercentage}%`
+                message.innerHTML = `You lost ${moneyLost}₹ which is about ${lossPercentage}%`
+                message.style.color = "#F87171";//red
+                lossImage.style.display = "block"
             } else {
-                message.innerHTML = "You did not make or loss any money"
+                message.innerHTML = "You did not make or lose any money"
+                message.style.color = "white"
             }
-        }, 000)
+        }, 2000)
     } else {
-        message.innerHTML = "Enter Valid input"
+        leftMessage.innerHTML = "Enter Valid input"
     }
 })
